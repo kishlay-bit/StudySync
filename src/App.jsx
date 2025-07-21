e6757e89528f8f0f5c803dff48c72fd071abdcf0
Kishlay <kishlay4305t@gmail.com>
2025-07-21 19:48:22 +0530

    Add all project files

---
+++ src/App.jsx
@@ -0,0 +1,318 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div className="head">
+        <header className="head1">
+            <div className="logo-container">
+                <a href="#" className="logo">
+                    <img id="mainlogo" src="/icon1.png" alt="icon" />
+                </a>
+                <a href="#" className="logo" id="t11">StudySync</a>
+            </div>
+            <div className="navbar">
+                <ul>
+                    <li className="navtext"><a href="#">Home</a></li>
+                    <li className="navtext"><a href="#">Features</a></li>
+                    <li className="navtext"><a href="#">Pricing</a></li>
+                    <li className="navtext"><a href="#">Blog</a></li>
+                    <li className="navtext"><a href="#">About</a></li>
+                </ul>
+            </div>
+            <div className="contact">
+                <a href="#" id="t12">Contact Us</a>
+            </div>
+            <button type="button" className="menu">
+                <img id="imgmenu" src="/menu.png" alt="menu" />
+            </button>
+        </header>
+    </div>
+
+    <div className="content">
+        <section className="main-outline">
+            <div className="content-left">
+                <div className="t00">
+                    <p id="t10">
+                        Very proud to introduce
+                    </p>
+                    <h1 id="t20">
+                        Seamless Learning for Bright Futures
+                    </h1> <br /><br />
+                    <p id="t30">
+                        Unveil the essence: a community-driven platform dedicated to empowering coders of all levels. Discover who we are and how we're shaping the future of coding education.
+                    </p>
+                </div><br /><br />
+                <div className="button101">
+                    <a href="#" id="b21">Start Now</a>
+                    <a href="#" id="b22">Take tour</a>
+                </div>
+            </div>
+            <div className="content-right">
+                <img src="/main2.jpg" alt="main" id="img101" />
+            </div>
+        </section>
+    </div>
+
+    <div className="trusted">
+        <div><h2 id="t301">Trusted by the best</h2></div>
+        <div className="company">
+            <div className="comp">
+                <img src="/google-logo.png" alt="Google" className="logo101" />
+                <span>Google</span>
+            </div>
+            <div className="comp">
+                <img src="/micro-logo.webp" alt="Microsoft" className="logo101" />
+                <span>Microsoft</span>
+            </div>
+            <div className="comp">
+                <img src="/linkdin-icon.png" alt="Linkedin" className="logo101" />
+                <span>Linkedin</span>
+            </div>
+            <div className="comp">
+                <img src="/harvard-logo.jpg" alt="Harvard" className="logo101" />
+                <span>HarvardEdu</span>
+            </div>
+            
+            
+            
+            
+
+        </div>
+    </div>
+
+    <div className="card">
+        <div className="text-card">
+            <h1 id="t401">
+                Our Competitive Advantage
+            </h1> <br />
+            <p id="t402">
+                This is a section of some simple text, also known as placeholder text. It shares some characteristics of real text but is randomly generated. 
+            </p>
+            <div className="card-main">
+                <div className="box" id="box1">
+                    <div>
+                        <img className="img401" src="/pers22.png" alt="Personalized Learning" />
+                    </div>
+                    <div className="t403">
+                        <h3 className="t404">
+                            Personalized Learning
+                        </h3>
+                        <p className="t405">Offers teachers training for multiple purposes. AI and machine learning to create a platform for individuals.</p>
+                    </div>
+                </div>
+
+                <div className="box" id="box2">
+                    <div>
+                        <img className="img401" src="/affordibility.webp" alt="Affordability" />
+                    </div>
+                    <div className="t403">
+                        <h3 className="t404">
+                            Affordability
+                        </h3>
+                        <p className="t405">Provides high-quality information at an affordable and genuine market rate, making it available for the mass audience.</p>
+                    </div>      
+                </div>
+                <div className="box" id="box3">
+                    <div>
+                        <img className="img401" src="/industry.webp" alt="Industry Partnership" />
+                    </div>
+                    <div className="t403">
+                        <h3 className="t404">
+                            Industry Partnership
+                        </h3>
+                        <p className="t405">Collaborate with well-known companies and institutions to offer accredited courses and certifications, adding credibility to your offerings.</p>
+                    </div>                    
+                </div>
+
+                <div className="box" id="box4">
+                    <div>
+                        <img className="img401" src="/Information-technology-icon-Graphics-13307570-1-1-580x386.jpg" alt="Information Technology" />
+                    </div>
+                    <div className="t403">
+                        <h3 className="t404">
+                            Information Technology
+                        </h3>
+                        <p className="t405">Utilize cutting edge technology, such as augmented reality or virtual reality, to create immersive learning experiences.</p>
+                    </div>                    
+                </div>
+
+                <div className="box" id="box5">
+                    <div>
+                        <img className="img401" src="/support.png" alt="Responsive Support" />
+                    </div>
+                    <div className="t403">
+                        <h3 className="t404">
+                            Responsive Support
+                        </h3>
+                        <p className="t405">Provides exceptional customer support and assistance to students and educators.</p>
+                    </div>                    
+                </div>
+
+                <div className="box" id="box6">
+                    <div>
+                        <img className="img401" src="/analytics.png" alt="Analytics and Insights" />
+                    </div>
+                    <div className="t403">
+                        <h3 className="t404">
+                            Analytics and Insights
+                        </h3>
+                        <p className="t405">Offer detailed progress tracking and analytics to help students and teachers monitor performance and make data-driven decisions.</p>
+                    </div>                    
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div className="about">
+        <h2 id="t501">What Others Say About Us</h2>
+        <div className="area501">
+            <div className="box501">
+                <p className="t502">
+                    "StudySync revolutionized my classroom. Engaging content and teacher resources make learning enjoyable. Highly recommended for educators."
+                </p> <br />
+                <img src="/re11.jpg" alt="Mark Davis" className="img501" /> <br />
+                <h3 className="t503">
+                    Mark Davis
+                </h3>
+                <p className="t504">
+                    8th Grade English Teacher
+                </p>
+            </div>
+            <div className="box501">
+                <p className="t502">
+                    "StudySync has made learning fun for my child. The content is engaging, and the resources really support great teaching. Highly recommended!"
+                </p> <br />
+                <img src="/re13.jpeg" alt="Sarah Johnson" className="img501" /> <br />
+                <h3 className="t503">
+                    Sarah Johnson
+                </h3>
+                <p className="t504">
+                    Parent
+                </p>
+            </div>
+            <div className="box501">
+                <p className="t502">
+                    "StudySync provides engaging content and valuable teacher resources that enhance our curriculum and improve student learning outcomes. A highly recommended tool for schools."
+                </p> <br />
+                <img src="/re12.jpeg" alt="Dr. James Carter" className="img501" /> <br />
+                <h3 className="t503">
+                    Dr. James Carter
+                </h3>
+                <p className="t504">
+                    School Administrator
+                </p>
+            </div>
+        </div>
+    </div>
+    
+    <div className="news">
+        <div className="newsletter">
+            <div className="img601">
+                <img src="/news101.jpg"  id="img602" alt="Newsletter" />
+            </div>
+            <div className="text601">
+                <div >
+                    <h2 id="t602">Get the latest updates</h2>
+                </div>
+                <div >
+                    <p className="t603">Sign up for our newsletter</p>
+                </div>
+                <div className="t604">
+                    <table>
+                        <tbody>
+                        <tr>
+                            <td>
+                                <input type="text" name="yourName" placeholder="Email" style={{ width: '120px', height: '24px' }} />
+                            </td>
+                            <td>
+                                <div className="button101">
+                                    <a href="#" id="b21">Send</a>
+                                </div>
+                            </td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <div >
+                    <p className="t603">By signing up to our newsletter you agree to our <a href="#">Terms of Service</a> and <a href="#">Privacy Policy</a></p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div className="footer">
+        <div className="foot1">
+            <div className="foot-left">
+                <div className="h701">
+                    <img src="/icon1.png" alt="StudySync Logo" id="img702" />
+                    <span id="h703">
+                        StudySync
+                    </span>
+                </div>
+                <p>Seamless Learning for Bright Futures</p> <br />
+                <div className="h801">
+                    <div className="icon801">
+                        <a href="#"><img src="/innsta-logo.png" alt="Instagram" className="logo101" /></a>
+                        <a href="#"><img src="/twitter-logo.png" alt="Twitter" className="logo101" /></a>
+                        <a href="#"><img src="/linkdin-icon.png" alt="Linkedin" className="logo101" /></a>
+                        <a href="#"><img src="/git-logo.png" alt="GitHub" className="logo101" /></a>
+                    </div>
+                </div>
+            </div>
+            <div className="foot-right">
+                <div className="list1">
+                    <h3 className="t811">
+                        Products
+                    </h3> <br />
+                    <ul className="t822">
+                        <li><a href="#" className="t812">Overview</a></li>
+                        <li><a href="#" className="t812">Solutions</a></li>
+                        <li><a href="#" className="t812">Pricing</a></li>
+                        <li><a href="#" className="t812">Customers</a></li>
+                    </ul>
+                </div>
+                <div className="list1">
+                    <h3 className="t811">
+                        Company
+                    </h3> <br />
+                    <ul className="t822">
+                        <li><a href="#" className="t812">About</a></li>
+                        <li><a href="#" className="t812">Investor Relations</a></li>
+                        <li><a href="#" className="t812">Jobs</a></li>
+                        <li><a href="#" className="t812">Press</a></li>
+                        <li><a href="#" className="t812">Blog</a></li>
+                    </ul>
+                </div>
+                <div className="list1">
+                    <h3 className="t811">
+                        Support
+                    </h3> <br />
+                    <ul className="t822">
+                        <li><a href="#" className="t812">Contact</a></li>
+                        <li><a href="#" className="t812">Documentation</a></li>
+                        <li><a href="#" className="t812">Chat</a></li>
+                        <li><a href="#" className="t812">FAQ</a></li>
+                    </ul>
+                </div>
+                <div className="list1">
+                    <h3 className="t811">
+                        Legal
+                    </h3> <br />
+                    <ul className="t822">
+                        <li><a href="#" className="t812">Terms of Service</a></li>
+                        <li><a href="#" className="t812">Privacy Policy</a></li>
+                        <li><a href="#" className="t812">Cookie Settings</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default App
